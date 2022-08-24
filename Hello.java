fd674c3d65244d788ba0584dc5f4132a47297871
Its4ry4n <112003532+Its4ry4n@users.noreply.github.com>
2022-08-24 12:03:57 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+// Hello Java
+// Aryan Rathi
+// 8/24/2022
+// CMSC255 004
+
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Aryan Rathi");
+		System.out.println("Hello Java");
+		System.out.println("an introductory hello world project.");
+		System.out.println("8/24/2022");
+		System.out.println("CMSC255 004");
+		System.out.println("");
+	}
+}
